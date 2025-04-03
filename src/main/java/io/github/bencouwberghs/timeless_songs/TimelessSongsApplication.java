e40a5f4d7fba52118dac8a9bbf8553954547fff3
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-04-03 18:26:38 +0200

    #1: Created an empty Spring project with Spring Data JPA, Web and Lombok dependencies added.

---
+++ src/main/java/io/github/bencouwberghs/timeless_songs/TimelessSongsApplication.java
@@ -0,0 +1,13 @@
+package io.github.bencouwberghs.timeless_songs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimelessSongsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimelessSongsApplication.class, args);
+	}
+
+}
