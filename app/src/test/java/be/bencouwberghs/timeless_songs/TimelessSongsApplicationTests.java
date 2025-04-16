1815026b3e49d16a1c2b2c9cede908efab996000
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-04-16 20:55:49 +0200

    #3: feat: moved test folder and application properties to app module.

--- app/src/test/java/be/bencouwberghs/timeless_songs/TimelessSongsApplicationTests.java
+++ app/src/test/java/be/bencouwberghs/timeless_songs/TimelessSongsApplicationTests.java
@@ -1,4 +1,4 @@
-package io.github.bencouwberghs.timeless_songs;
+package be.bencouwberghs.timeless_songs;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
