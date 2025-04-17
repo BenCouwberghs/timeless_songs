4b7b2b04a03bb8d54fff234131477482c8294a45
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-04-17 16:53:46 +0200

    #3: Feat: Created empty BandRepository class and added the model module as dependency to repository module.

---
+++ repository/src/main/java/be/bencouwberghs/timeless_songs/repository/BandRepository.java
@@ -0,0 +1,7 @@
+package be.bencouwberghs.timeless_songs.repository;
+
+import be.bencouwberghs.timeless_songs.model.Band;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BandRepository extends JpaRepository<Band, Long> {
+}
