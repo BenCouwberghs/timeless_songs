df0723c7bc1a32e36782813172f7742a67caecc0
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-04-17 19:36:21 +0200

    #3: Feat: implementation of BandRepository

--- repository/src/main/java/be/bencouwberghs/timeless_songs/repository/BandRepository.java
+++ repository/src/main/java/be/bencouwberghs/timeless_songs/repository/BandRepository.java
@@ -4,4 +4,5 @@ import be.bencouwberghs.timeless_songs.model.Band;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BandRepository extends JpaRepository<Band, Long> {
+    boolean existsByName(String name);
 }
