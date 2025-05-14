d3108a99597c5eaeb9537323f4019e50819060cd
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-05-14 20:16:35 +0200

    #4: feat: Added the remaining parts of the integration test for Band.

--- app/src/test/java/be/bencouwberghs/timeless_songs/BandIntegrationTest.java
+++ app/src/test/java/be/bencouwberghs/timeless_songs/BandIntegrationTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
@@ -32,5 +33,51 @@ public class BandIntegrationTest {
         bandService.addBand(band3);
 
         assertNotNull(band3.getId());
+    }
+
+    @Test
+    void modifyBand() {
+        Band band4 = new Band();
+
+        band4.setName("band 4");
+        band4.setLinkWikiPage("testlink4");
+
+        bandService.addBand(band4);
+
+        band4.setName("Beatles");
+        bandService.modifyBand(band4);
+
+        assertEquals(band4, bandService.findBandByName("Beatles"));
+    }
+
+    @Test
+    void deleteBand() {
+        Band band5 = new Band();
+
+        band5.setName("band 5");
+        band5.setLinkWikiPage("testlink5");
+
+        bandService.addBand(band5);
+        bandService.deleteBand(band5);
+
+        assertEquals(0, bandService.fetchAllBands().size());
+    }
+
+    @Test
+    void fetchAllBands() {
+        Band band1 = new Band();
+
+        band1.setName("band 1");
+        band1.setLinkWikiPage("testlink1");
+
+        Band band2 = new Band();
+
+        band2.setName("band 2");
+        band2.setLinkWikiPage("testlink2");
+
+        bandService.addBand(band1);
+        bandService.addBand(band2);
+
+        assertEquals(2, bandService.fetchAllBands().size());
     }
 }
