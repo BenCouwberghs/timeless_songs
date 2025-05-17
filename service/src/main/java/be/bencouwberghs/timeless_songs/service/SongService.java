d74899949253739e7e5022685cdcbb61dbeeab98
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-05-17 23:29:34 +0200

    #8: Feat: Implemented SongService interface.

---
+++ service/src/main/java/be/bencouwberghs/timeless_songs/service/SongService.java
@@ -0,0 +1,14 @@
+package be.bencouwberghs.timeless_songs.service;
+
+import be.bencouwberghs.timeless_songs.model.Band;
+import be.bencouwberghs.timeless_songs.model.Song;
+
+import java.util.List;
+
+public interface SongService {
+    void addSong(Song song);
+    void modifySong(Song song);
+    void deleteSong(Song song);
+    List<Song> fetchAllSongs();
+    List<Song> fetchAllSongsOfBand();
+}
