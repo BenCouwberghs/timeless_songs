27c6c3db56aabfb9978a75960b56bf5d90c11376
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-06-10 16:11:10 +0200

    #10: Feat: Completed unit tests for MapperEntities.

--- service/src/test/java/be/bencouwberghs/timeless_songs/service/mapper/MapperEntitiesTest.java
+++ service/src/test/java/be/bencouwberghs/timeless_songs/service/mapper/MapperEntitiesTest.java
@@ -1,16 +1,46 @@
 package be.bencouwberghs.timeless_songs.service.mapper;
 
+import be.bencouwberghs.timeless_songs.model.Band;
+import be.bencouwberghs.timeless_songs.model.dto.BandDto;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MapperEntitiesTest {
 
+    MapperEntities mapperEntities = new MapperEntities();
+
     @Test
     void mapBandEntityToDto() {
+        Band band = new Band() {{
+            setId(1L);
+            setName("band 1");
+            setLinkWikiPage("testLink 1");
+        }};
+
+        BandDto expected = BandDto.builder()
+                .id(1L)
+                .name("band 1")
+                .wikiLinkPage("testLink 1")
+                .build();
+
+        assertEquals(expected, mapperEntities.mapBandEntityToDto(band));
     }
 
     @Test
     void mapBandDtoToBandEntity() {
+        Band expected = new Band() {{
+            setId(2L);
+            setName("band 2");
+            setLinkWikiPage("testLink 2");
+        }};
+
+        BandDto bandDto = BandDto.builder()
+                .id(2L)
+                .name("band 2")
+                .wikiLinkPage("testLink 2")
+                .build();
+
+        assertEquals(expected, mapperEntities.mapBandDtoToBandEntity(bandDto));
     }
 }
