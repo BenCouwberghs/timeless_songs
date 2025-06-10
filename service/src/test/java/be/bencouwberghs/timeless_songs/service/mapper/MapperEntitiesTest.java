2b5d6f1e8b70326ad685e3e3761ef3a64b851579
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-06-10 15:59:05 +0200

    #10: Feat: Generate unit tests for ValidateEntities and MapperEntities.

---
+++ service/src/test/java/be/bencouwberghs/timeless_songs/service/mapper/MapperEntitiesTest.java
@@ -0,0 +1,16 @@
+package be.bencouwberghs.timeless_songs.service.mapper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MapperEntitiesTest {
+
+    @Test
+    void mapBandEntityToDto() {
+    }
+
+    @Test
+    void mapBandDtoToBandEntity() {
+    }
+}
---
+++ service/src/test/java/be/bencouwberghs/timeless_songs/service/validator/ValidateEntitiesTest.java
@@ -0,0 +1,12 @@
+package be.bencouwberghs.timeless_songs.service.validator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidateEntitiesTest {
+
+    @Test
+    void validateBand() {
+    }
+}
