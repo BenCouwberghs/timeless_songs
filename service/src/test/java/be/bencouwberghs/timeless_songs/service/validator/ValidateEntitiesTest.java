d607b7de59ed25e030a737c074da1cd5be1143f1
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-06-25 19:37:23 +0200

    #11: Feat: Added unit tests for the new methods in ValidateEntities.

--- service/src/test/java/be/bencouwberghs/timeless_songs/service/validator/ValidateEntitiesTest.java
+++ service/src/test/java/be/bencouwberghs/timeless_songs/service/validator/ValidateEntitiesTest.java
@@ -1,6 +1,7 @@
 package be.bencouwberghs.timeless_songs.service.validator;
 
 import be.bencouwberghs.timeless_songs.model.dto.BandDto;
+import be.bencouwberghs.timeless_songs.model.dto.SongDto;
 import be.bencouwberghs.timeless_songs.service.exception.UserInputException;
 import org.junit.jupiter.api.Test;
 
@@ -22,10 +23,30 @@ class ValidateEntitiesTest {
     @Test
     void validateBandPass() {
         BandDto bandDto = BandDto.builder()
-                .name("band e")
+                .name("band 2")
                 .wikiLinkPage("testLink 2")
                 .build();
 
         assertDoesNotThrow(() -> validateEntities.validateBand(bandDto));
+    }
+
+    @Test
+    void validateSongFail() {
+        SongDto songDto = SongDto.builder()
+                .name("song 1")
+                .build();
+
+        assertThrows(UserInputException.class, () -> validateEntities.validateSong(songDto));
+    }
+
+    @Test
+    void validateSongPass() {
+        SongDto songDto = SongDto.builder()
+                .name("song 2")
+                .year(1995)
+                .wikiLinkPage("testLink 2")
+                .build();
+
+        assertDoesNotThrow(() -> validateEntities.validateSong(songDto));
     }
 }
