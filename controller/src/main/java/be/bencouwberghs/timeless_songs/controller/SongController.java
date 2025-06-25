dd8c9dfea2d01083c5c35dd9f35b90802ef3b661
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-06-25 19:25:13 +0200

    #11: Feat: Added the remaining methods required in the SongController

--- controller/src/main/java/be/bencouwberghs/timeless_songs/controller/SongController.java
+++ controller/src/main/java/be/bencouwberghs/timeless_songs/controller/SongController.java
@@ -9,9 +9,9 @@ import be.bencouwberghs.timeless_songs.service.mapper.MapperEntities;
 import be.bencouwberghs.timeless_songs.service.validator.ValidateEntities;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -34,5 +34,37 @@ public class SongController {
         } catch (UserInputException userInputException) {
             return ResponseEntity.badRequest().body(userInputException.getMessage());
         }
+    }
+
+    @PatchMapping("/songs")
+    public ResponseEntity<String> modifySong(@RequestBody SongDto songDto) {
+        try {
+            validateEntities.validateSong(songDto);
+            Song newSong = mapperEntities.mapSongDtoToSongEntity(songDto);
+            songService.modifySong(newSong);
+            return ResponseEntity.ok("Successfully updated song.");
+        } catch (UserInputException userInputException) {
+            return ResponseEntity.badRequest().body(userInputException.getMessage());
+        }
+    }
+
+    @DeleteMapping("/songs/{id}")
+    public ResponseEntity<String> deleteSong(@PathVariable Long id) {
+        try {
+            songService.deleteSong(songRepository.getReferenceById(id));
+            return ResponseEntity.ok("Successfully deleted song.");
+        } catch (Exception e) {
+            return  ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/songs")
+    public List<Song> getAllSongs() {
+        return songService.fetchAllSongs();
+    }
+
+    @GetMapping("/songs/{id}")
+    public Song getSong(@PathVariable Long id) {
+        return songRepository.getReferenceById(id);
     }
 }
