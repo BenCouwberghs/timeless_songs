0fc5e93fdd78056e78c544b4fca1584d935f1e7d
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-10-10 20:01:36 +0200

    #82: Feat: Updated SongServiceImplTest to have all tests work again.

--- service/src/test/java/be/bencouwberghs/timeless_songs/service/SongServiceImplTest.java
+++ service/src/test/java/be/bencouwberghs/timeless_songs/service/SongServiceImplTest.java
@@ -2,7 +2,9 @@ package be.bencouwberghs.timeless_songs.service;
 
 import be.bencouwberghs.timeless_songs.model.Band;
 import be.bencouwberghs.timeless_songs.model.Song;
+import be.bencouwberghs.timeless_songs.model.dto.SongDto;
 import be.bencouwberghs.timeless_songs.repository.SongRepository;
+import be.bencouwberghs.timeless_songs.service.mapper.MapperEntities;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -10,6 +12,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
@@ -21,32 +24,52 @@ public class SongServiceImplTest {
     @Mock
     private SongRepository songRepository;
 
+    @Mock
+    private MapperEntities mapperEntities;
+
     @InjectMocks
     private SongServiceImpl songService;
 
     @Test
     void addSong() {
+        SongDto songDto1 = new SongDto() {{
+            setId(1L);
+            setName("song1");
+            setYear(1990);
+        }};
+
         Song song1 = new Song() {{
             setId(1L);
             setName("song1");
             setYear(1990);
         }};
 
-        songService.addSong(song1);
+        when(mapperEntities.mapSongDtoToSongEntity(songDto1)).thenReturn(song1);
+        songService.addSong(songDto1);
         verify(songRepository).save(song1);
     }
 
     @Test
     void modifySong() {
+        SongDto songDto2 = new SongDto() {{
+            setId(2L);
+            setName("song2");
+            setYear(1990);
+        }};
+
         Song song2 = new Song() {{
             setId(2L);
             setName("song2");
             setYear(1990);
         }};
 
+        when(songRepository.findById(songDto2.getId())).thenReturn(Optional.of(song2));
         when(songRepository.existsByNameAndIdNot(song2.getName(), song2.getId())).thenReturn(false);
+        when(mapperEntities.updateSongEntityFromDto(song2, songDto2)).thenReturn(song2);
 
-        songService.modifySong(song2);
+        songService.modifySong(songDto2, song2.getId());
+
+        verify(mapperEntities).updateSongEntityFromDto(song2, songDto2);
         verify(songRepository).save(song2);
     }
 
@@ -58,7 +81,7 @@ public class SongServiceImplTest {
             setYear(1990);
         }};
 
-        when(songRepository.getReferenceById(song3.getId())).thenReturn(song3);
+        when(songRepository.findById(song3.getId())).thenReturn(Optional.of(song3));
 
         songService.deleteSongById(song3.getId());
         verify(songRepository).delete(song3);
@@ -66,19 +89,19 @@ public class SongServiceImplTest {
 
     @Test
     void fetchAllSongs() {
-        Song song4 = new Song() {{
+        SongDto songDto4 = new SongDto() {{
             setId(4L);
             setName("song4");
             setYear(1990);
         }};
 
-        Song song5 = new Song() {{
+        SongDto songDto5 = new SongDto() {{
             setId(5L);
             setName("song5");
             setYear(1990);
         }};
 
-        when(songRepository.findAll()).thenReturn(List.of(song4, song5));
+        when(mapperEntities.mapSongEntitiesToDtos(songRepository.findAll())).thenReturn(List.of(songDto4, songDto5));
         var songList = songService.fetchAllSongs();
 
         assertThat(songList).isNotNull();
@@ -92,21 +115,21 @@ public class SongServiceImplTest {
             setName("band");
         }};
 
-        Song song4 = new Song() {{
+        SongDto songDto4 = new SongDto() {{
             setId(4L);
             setName("song4");
             setYear(1990);
             setBand(band);
         }};
 
-        Song song5 = new Song() {{
+        SongDto songDto5 = new SongDto() {{
             setId(5L);
             setName("song5");
             setYear(1990);
             setBand(band);
         }};
 
-        when(songRepository.findAllByBand(band)).thenReturn(List.of(song4, song5));
+        when(mapperEntities.mapSongEntitiesToDtos(songRepository.findAllByBand(band))).thenReturn(List.of(songDto4, songDto5));
         var songList = songService.fetchAllSongsOfBand(band);
 
         assertThat(songList).isNotNull();
