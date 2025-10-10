3c0235ca8950722e590e659ee2bc5b8067ed1b3d
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-10-10 16:44:07 +0200

    #82: Feat: Updated SongService & Impl to only sent and receive Dtos.

--- service/src/main/java/be/bencouwberghs/timeless_songs/service/SongService.java
+++ service/src/main/java/be/bencouwberghs/timeless_songs/service/SongService.java
@@ -2,14 +2,15 @@ package be.bencouwberghs.timeless_songs.service;
 
 import be.bencouwberghs.timeless_songs.model.Band;
 import be.bencouwberghs.timeless_songs.model.Song;
+import be.bencouwberghs.timeless_songs.model.dto.SongDto;
 
 import java.util.List;
 
 public interface SongService {
-    void addSong(Song song);
-    void modifySong(Song song);
+    void addSong(SongDto songDto);
+    void modifySong(SongDto songDto, Long id);
     void deleteSongById(Long id);
-    Song fetchSong(Long id);
-    List<Song> fetchAllSongs();
-    List<Song> fetchAllSongsOfBand(Band band);
+    SongDto fetchSong(Long id);
+    List<SongDto> fetchAllSongs();
+    List<SongDto> fetchAllSongsOfBand(Band band);
 }
