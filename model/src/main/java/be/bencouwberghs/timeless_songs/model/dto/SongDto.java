20421da18dac154557bc8bb6f82655255e6b801e
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-10-10 16:51:28 +0200

    #82: Feat: Added No & AllArgsInstructor annotations to SongDto.

--- model/src/main/java/be/bencouwberghs/timeless_songs/model/dto/SongDto.java
+++ model/src/main/java/be/bencouwberghs/timeless_songs/model/dto/SongDto.java
@@ -1,11 +1,15 @@
 package be.bencouwberghs.timeless_songs.model.dto;
 
 import be.bencouwberghs.timeless_songs.model.Band;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SongDto {
     private Long id;
     private String name;
