e859514c19e31c759a6d90a58c7832f057ab4053
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-11-03 23:41:07 +0100

    #98: Fix: Removed test method and modified getAllGenres test from received feedback.

--- controller/src/test/java/be/bencouwberghs/timeless_songs/controller/GenreControllerTest.java
+++ controller/src/test/java/be/bencouwberghs/timeless_songs/controller/GenreControllerTest.java
@@ -1,6 +1,5 @@
 package be.bencouwberghs.timeless_songs.controller;
 
-import be.bencouwberghs.timeless_songs.model.dto.GenreDto;
 import be.bencouwberghs.timeless_songs.service.GenreService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,10 +7,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.List;
-
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class GenreControllerTest {
@@ -23,35 +19,8 @@ public class GenreControllerTest {
     private GenreController genreController;
 
     @Test
-    void getGenre() {
-        GenreDto genreDto1 = GenreDto.builder()
-                .id(1L)
-                .description("testDescription 1")
-                .build();
-
-        Long id = 1L;
-
-        when(genreService.fetchGenre(id)).thenReturn(genreDto1);
-        genreController.getGenre(id);
-
-        verify(genreService).fetchGenre(id);
-    }
-
-    @Test
     void getAllGenres() {
-        GenreDto genreDto2 = GenreDto.builder()
-                .id(2L)
-                .description("testDescription 2")
-                .build();
-
-        GenreDto genreDto3 = GenreDto.builder()
-                .id(3L)
-                .description("testDescription 3")
-                .build();
-
-        when(genreService.fetchAllGenres()).thenReturn(List.of(genreDto2, genreDto3));
         genreController.getAllGenres();
-
         verify(genreService).fetchAllGenres();
     }
 }
