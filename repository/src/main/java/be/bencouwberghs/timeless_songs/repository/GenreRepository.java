b67dc8aea159865dcb8c239ecc87252f7e7865bf
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-11-03 20:27:22 +0100

    #98: Feat: Added GenreRepository.

---
+++ repository/src/main/java/be/bencouwberghs/timeless_songs/repository/GenreRepository.java
@@ -0,0 +1,10 @@
+package be.bencouwberghs.timeless_songs.repository;
+
+import be.bencouwberghs.timeless_songs.model.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+    Genre findByDescription(String description);
+}
