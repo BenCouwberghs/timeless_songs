7aa5b75f811c986cd821c985655e9aa8df79ff23
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-11-05 19:47:16 +0100

    #88: Feat: Made changes so API_KEY actually gets assigned.

--- external-services/src/main/java/be/bencouwberghs/timeless_songs/external/service/YouTubeSearchService.java
+++ external-services/src/main/java/be/bencouwberghs/timeless_songs/external/service/YouTubeSearchService.java
@@ -6,20 +6,24 @@ import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.SearchListResponse;
 import com.google.api.services.youtube.model.SearchResult;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.List;
 
+@Service
 public class YouTubeSearchService {
 
     private static final String APPLICATION_NAME = "YouTubeSearchApp";
-    private static final String API_KEY = System.getenv("API_KEY");
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
 
+    private String API_KEY;
     private YouTube youtubeService;
 
-    public YouTubeSearchService() throws GeneralSecurityException, IOException {
+    public YouTubeSearchService(@Value("${youTube.api.key}") String API_KEY) throws GeneralSecurityException, IOException {
+        this.API_KEY = API_KEY;
         youtubeService = new YouTube.Builder(
                 GoogleNetHttpTransport.newTrustedTransport(),
                 JSON_FACTORY,
