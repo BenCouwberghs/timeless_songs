8ca52d4462ce3c07f76a7d7d183a0c04d310f423
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-11-06 15:53:37 +0100

    #104: Fix: Added null checks.

--- service/src/main/java/be/bencouwberghs/timeless_songs/service/SearchVideosServiceImpl.java
+++ service/src/main/java/be/bencouwberghs/timeless_songs/service/SearchVideosServiceImpl.java
@@ -24,21 +24,43 @@ public class SearchVideosServiceImpl implements SearchVideosService{
 
         List<SearchVideoResult> searchVideoResults = new ArrayList<>();
         for (SearchResult searchResult : searchResults) {
-            SearchVideoResult searchVideoResult = SearchVideoResult.builder()
-                    .source("youtube")
-                    .title(searchResult.getSnippet().getTitle())
-                    .videoId(searchResult.getId().getVideoId())
-                    .build();
+            SearchVideoResult.SearchVideoResultBuilder searchVideoResultBuilder = SearchVideoResult.builder();
+            searchVideoResultBuilder.source("youtube");
+            if (searchResult.getSnippet().getTitle() != null) {
+                searchVideoResultBuilder.title(searchResult.getSnippet().getTitle());
+            }
+
+            if (searchResult.getId().getVideoId() != null) {
+                searchVideoResultBuilder.videoId(searchResult.getId().getVideoId());
+            }
+
+            SearchVideoResult searchVideoResult = searchVideoResultBuilder.build();
 
             ThumbnailDetails thumbnailDetails = searchResult.getSnippet().getThumbnails();
-            SearchVideoClipThumbnails searchVideoClipThumbnails = SearchVideoClipThumbnails.builder()
-                    .urlDefault(thumbnailDetails.getDefault().getUrl())
-                    .urlMedium(thumbnailDetails.getMedium().getUrl())
-                    .urlHigh(thumbnailDetails.getHigh().getUrl())
-                    .urlStandard(thumbnailDetails.getStandard().getUrl())
-                    .urlMaxRes(thumbnailDetails.getMaxres().getUrl())
-                    .build();
+            SearchVideoClipThumbnails.SearchVideoClipThumbnailsBuilder searchVideoClipThumbnailsBuilder =
+                    SearchVideoClipThumbnails.builder();
+            if (thumbnailDetails.getDefault() != null) {
+                searchVideoClipThumbnailsBuilder.urlDefault(thumbnailDetails.getDefault().getUrl());
+            }
 
+            if (thumbnailDetails.getMedium() != null) {
+                searchVideoClipThumbnailsBuilder.urlMedium(thumbnailDetails.getMedium().getUrl());
+            }
+
+            if (thumbnailDetails.getHigh() != null) {
+                searchVideoClipThumbnailsBuilder.urlHigh(thumbnailDetails.getHigh().getUrl());
+            }
+
+            if (thumbnailDetails.getStandard() != null) {
+                searchVideoClipThumbnailsBuilder.urlStandard(thumbnailDetails.getStandard().getUrl());
+            }
+
+            if (thumbnailDetails.getMaxres() != null) {
+                searchVideoClipThumbnailsBuilder.urlMaxRes(thumbnailDetails.getMaxres().getUrl());
+            }
+
+
+            SearchVideoClipThumbnails searchVideoClipThumbnails = searchVideoClipThumbnailsBuilder.build();
             searchVideoResult.setThumbnails(searchVideoClipThumbnails);
             searchVideoResults.add(searchVideoResult);
         }
