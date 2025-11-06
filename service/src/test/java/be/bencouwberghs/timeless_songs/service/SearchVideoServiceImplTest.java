bac03296b7fdd00e3058eeddf269922ca1a188ec
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-11-06 16:46:04 +0100

    #104: Feat: Updated the unit test.

--- service/src/test/java/be/bencouwberghs/timeless_songs/service/SearchVideoServiceImplTest.java
+++ service/src/test/java/be/bencouwberghs/timeless_songs/service/SearchVideoServiceImplTest.java
@@ -1,6 +1,9 @@
 package be.bencouwberghs.timeless_songs.service;
 
 import be.bencouwberghs.timeless_songs.external.service.YouTubeSearchService;
+import be.bencouwberghs.timeless_songs.model.SearchVideoResult;
+import be.bencouwberghs.timeless_songs.service.mapper.MapSearchVideoResult;
+import com.google.api.services.youtube.model.SearchResult;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -8,13 +11,19 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class SearchVideoServiceImplTest {
     @Mock
     private YouTubeSearchService youTubeSearchService;
+
+    @Mock
+    private MapSearchVideoResult mapSearchVideoResult;
 
     @InjectMocks
     private SearchVideosServiceImpl searchVideosService;
@@ -24,7 +33,16 @@ public class SearchVideoServiceImplTest {
         String bandName = "The Beatles";
         String songName = "Let it be";
 
+        SearchResult searchResult = new SearchResult();
+
+        when(youTubeSearchService.searchByBandAndSong(bandName, songName))
+                .thenReturn(List.of(searchResult));
+
+        when(mapSearchVideoResult.map(searchResult)).thenReturn(SearchVideoResult.builder().build());
+
         searchVideosService.searchByBandAndSong(bandName, songName);
+
         verify(youTubeSearchService).searchByBandAndSong(bandName, songName);
+        verify(mapSearchVideoResult).map(searchResult);
     }
 }
