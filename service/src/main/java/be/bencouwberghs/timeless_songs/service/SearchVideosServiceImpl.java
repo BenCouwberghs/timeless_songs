b4f66bc3394f1a29b6d5560c1d3d2a8054e83852
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-11-06 17:05:18 +0100

    #104: Feat: refactor modifications done.

--- service/src/main/java/be/bencouwberghs/timeless_songs/service/SearchVideosServiceImpl.java
+++ service/src/main/java/be/bencouwberghs/timeless_songs/service/SearchVideosServiceImpl.java
@@ -3,7 +3,6 @@ package be.bencouwberghs.timeless_songs.service;
 import be.bencouwberghs.timeless_songs.external.service.YouTubeSearchService;
 import be.bencouwberghs.timeless_songs.model.SearchVideoResult;
 import be.bencouwberghs.timeless_songs.service.mapper.MapSearchVideoResult;
-import com.google.api.services.youtube.model.SearchResult;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -12,18 +11,13 @@ import java.util.List;
 @Service
 public class SearchVideosServiceImpl implements SearchVideosService{
     private final YouTubeSearchService youTubeSearchService;
-    private final MapSearchVideoResult mapSearchVideoResult;
 
-    public SearchVideosServiceImpl(YouTubeSearchService youTubeSearchService, MapSearchVideoResult mapSearchVideoResult) {
+    public SearchVideosServiceImpl(YouTubeSearchService youTubeSearchService) {
         this.youTubeSearchService = youTubeSearchService;
-        this.mapSearchVideoResult = mapSearchVideoResult;
     }
 
     public List<SearchVideoResult> searchByBandAndSong(String bandName, String songName) throws IOException {
-        List<SearchResult> searchResults = youTubeSearchService.searchByBandAndSong(bandName, songName);
-
-        return searchResults.stream()
-                .map(mapSearchVideoResult::map)
-                .toList();
+        return MapSearchVideoResult.map(youTubeSearchService
+                .searchByBandAndSong(bandName, songName));
     }
 }
