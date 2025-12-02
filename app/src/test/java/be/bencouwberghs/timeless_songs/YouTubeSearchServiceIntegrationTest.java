37c66248dcf7968e82e0f8b050afdf99a8c8ed4b
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-12-02 12:52:57 +0100

    #111: Feat: Created integration test for findContentDetails method.

--- app/src/test/java/be/bencouwberghs/timeless_songs/YouTubeSearchServiceIntegrationTest.java
+++ app/src/test/java/be/bencouwberghs/timeless_songs/YouTubeSearchServiceIntegrationTest.java
@@ -2,6 +2,7 @@ package be.bencouwberghs.timeless_songs;
 
 import be.bencouwberghs.timeless_songs.external.service.YouTubeSearchService;
 import com.google.api.services.youtube.model.SearchResult;
+import com.google.api.services.youtube.model.VideoContentDetails;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +11,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -33,5 +35,22 @@ public class YouTubeSearchServiceIntegrationTest {
 
         assertNotNull(results);
         assertFalse(results.isEmpty());
+    }
+
+    @Test
+    void findContentDetails() throws IOException {
+        String bandName = "The Beatles";
+        String songName = "Let It Be";
+
+        List<SearchResult> results = youTubeSearchService.searchByBandAndSong(bandName, songName);
+        List<String> videoIds = new ArrayList<>();
+
+        videoIds.add(results.getFirst().getId().getVideoId());
+        videoIds.add(results.get(1).getId().getVideoId());
+
+        List<VideoContentDetails> contentDetails = youTubeSearchService.findContentDetails(videoIds);
+
+        assertNotNull(contentDetails);
+        assertFalse(contentDetails.isEmpty());
     }
 }
