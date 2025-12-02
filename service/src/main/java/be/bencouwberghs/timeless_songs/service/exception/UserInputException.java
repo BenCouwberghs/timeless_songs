e45550122b98807217ed2b32c58689491137c091
Ben Couwberghs <ben.couwberghs@gmail.com>
2025-12-02 17:27:57 +0100

    #117: Feat: Changed UserInputException to extend from RuntimeException.

--- service/src/main/java/be/bencouwberghs/timeless_songs/service/exception/UserInputException.java
+++ service/src/main/java/be/bencouwberghs/timeless_songs/service/exception/UserInputException.java
@@ -1,6 +1,6 @@
 package be.bencouwberghs.timeless_songs.service.exception;
 
-public class UserInputException extends Exception {
+public class UserInputException extends RuntimeException {
     public UserInputException(String userInputException) {
         super(userInputException);
     }
